ad5a81c8b63144383928e591825a823b6b8c1ace
MorshedSayeed <78271544+MorshedSayeed@users.noreply.github.com>
2021-01-31 03:25:47 +0600

    First Commit
    Added Unity Project to Github

---
+++ Human_body- Movement/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Animator Anim;
+	// Start is called before the first frame update
+    void Start()
+    {
+        Anim = gameObject.GetComponent<Animator> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown (KeyCode.UpArrow)){
+		Anim.SetBool ("isWalking", true);
+	}
+	
+	else if(Input.GetKeyUp (KeyCode.UpArrow)){
+		Anim.SetBool ("isWalking", false);
+	}
+
+	if (Input.GetKeyDown (KeyCode.Space)){
+		Anim.SetBool ("isJumping", true);
+	}
+
+	else if (Input.GetKeyUp (KeyCode.Space)){
+		Anim.SetBool ("isJumping", false);
+	}
+    }
+}
